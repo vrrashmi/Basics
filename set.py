f8f7d6c3d1314d693624647fa5c200b5e048cc31
vrrashmi <40880775+vrrashmi@users.noreply.github.com>
2018-07-06 11:12:50 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,8 @@
+set1=set([1,2,3,4,5,6])
+set2=set([4,5,6,7,8,9])
+print("Union Representation")
+print(set1.union(set2))
+print("Intersection Representation")
+print(set1.intersection(set2))
+print("Symmetric difference Representation")
+print(set1 - set2)
