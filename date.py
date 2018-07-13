7bd4c55e5c0ec6e41703b8e1bf609ec69c7d9ebe
vrrashmi <40880775+vrrashmi@users.noreply.github.com>
2018-07-13 11:25:24 +0530

    Add files via upload

--- date.py
+++ date.py
@@ -1,5 +1,20 @@
+#Date and time program
+#Date : 13th July,2018
 import datetime
-t=datetime.time(1,2,3)
-print(" hour : ",t.hour)
-print(" min  : ",t.minute)
-print(" sec  : ",t.second)
+from datetime import date
+from datetime import timedelta
+v=timedelta(microseconds=-1)    #to get current time
+t=datetime.time(1,2,3)          #random time
+now=date.today()                #current date
+bd=date(1998,2,16)              #random birth date
+c=now-bd                        #difference of dates and time together
+
+#print all values
+print("Current time : ",v)
+print("Date now  : ",now)   
+print("Hour      : ",t.hour)
+print("Minutes   : ",t.minute)
+print("Seconds   : ",t.second)
+print("Current Time    :",v)
+print("Date and time difference : ",c)
+
